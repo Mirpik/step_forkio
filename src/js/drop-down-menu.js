4739e3105fa00724c9a74ae498f3d85755741562
Buglak <menonir@gmail.com>
2019-03-09 21:41:37 +0200

    finish-header

--- src/js/drop-down-menu.js
+++ src/js/drop-down-menu.js
@@ -2,7 +2,13 @@
 function toogleShowMenu() {
     let elem = document.getElementById('header-menu');
     let burgerButton = document.getElementById('burger-menu-btn');
-    elem.style.display = 'none'
+    console.log(window.screen.width);
+    
+    if(window.screen.width < 480) {
+        elem.style.display = 'none';
+    } else {
+        elem.style.display = 'flex';
+    }
     
     burgerButton.addEventListener('click', () => {
         if(elem.style.display == 'none') {
